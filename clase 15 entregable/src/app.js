f6daf7954bc7183f46d44ef430e723cd8f8f30f0
Munozmon004 <117871806+Munozmon004@users.noreply.github.com>
2023-07-06 14:17:39 -0500

    Add files via upload

---
+++ clase 15 entregable/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import handlebars from "express-handlebars";
+import mongoose from "mongoose";
+
+import viewsRouter from "./routes/views.router.js";
+import companiesRouter from "./routes/companies.router.js";
+
+import __dirname from "./utils.js";
+
+const app = express();
+const connection = await mongoose.connect(
+    "mongodb+srv://munozmon004:coder@cluster0.dwpogov.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+
+app.use("/", viewsRouter); 
+app.use("/api/companies", companiesRouter);
+
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
